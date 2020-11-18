eba33f72c5d1b87af27e1f63eb56361905d069b7
Guang-Yu6 <everyting-day@foxmail.com>
2020-11-18 18:32:33 +0800

    Money 页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,10 @@
 <template>
-<div>主页
-<Nav/>
-</div>
+  <div class="nav-wrapper">
+    <div class="content">
+      钱主页.vue
+    </div>
+  <Nav/>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,5 +14,15 @@ name: "Money",
 </script>
 
 <style lang="scss" scoped>
-
+  .nav-wrapper{
+    border: 5px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+  .content{
+    border: 5px solid saddlebrown;
+    overflow: auto;
+    flex-grow: 1;  // 尽量把高度都给这个content
+  }
 </style>
