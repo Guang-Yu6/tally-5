332f44c4f10cda3e3f7dfb8348ea3b239ecac7f1
Guang-Yu6 <everyting-day@foxmail.com>
2020-11-18 17:04:47 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>404,当前页面不存在，请检查网址是否正确</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+      <hr/>
+      <a href="/">也是返回首页</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+name: "NotFound"
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
