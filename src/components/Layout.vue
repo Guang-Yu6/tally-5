cc619ae0d1ce600b4de933c7e16968f584aa9bbc
Guang-Yu6 <everyting-day@foxmail.com>
2020-11-19 17:04:09 +0800

    添加 Nav 样式

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -15,13 +15,13 @@ export default {
 
 <style lang="scss" scoped>
     .nav-wrapper{
-      border: 5px solid green;
+
       display: flex;
       flex-direction: column;
       height: 100vh;
     }
     .content{
-      border: 5px solid saddlebrown;
+
       overflow: auto;
       flex-grow: 1;  // 尽量把高度都给这个content
     }
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,21 @@
 <template>
 
-  <div class="nav">
-    <router-link to="/money" >
+  <nav>
+    <router-link to="/money" class="item">
       <Icon name="money2"/>
       记账
     </router-link>
-    |
-    <router-link to="labels" >
+
+    <router-link to="labels" class="item">
       <Icon name="label2"/>
       标签
     </router-link>
-    |
-    <router-link to="statistics" >
+
+    <router-link to="statistics" class="item">
       <Icon name="statistics2"/>
       统计
     </router-link>
-  </div>
+  </nav>
 
 </template>
 
@@ -27,8 +27,23 @@ name: "Nav"
 </script>
 
 <style lang="scss" scoped>
-.nav{
-  border: 5px solid red;
-}
+  nav {
+  display: flex;
+    box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);  // 阴影
+    flex-direction: row;
+    font-size: 12px;
+    > .item{
+      padding: 2px 0;
+      width: 33.33333%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      .icon {
+        width: 32px;
+        height: 32px;
+      }
+    }
+  }
 
 </style>
