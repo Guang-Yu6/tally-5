53988b26db3c140b8bed5bd039fd47c60073fe63
Guang-Yu6 <everyting-day@foxmail.com>
2020-11-24 19:09:31 +0800

    完成 types 组件

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -12,17 +12,17 @@
 
 <script lang="ts">
   import  Vue from 'vue'
-  import {Component} from 'vue-property-decorator';  // 装饰器
+  import {Component, Prop} from 'vue-property-decorator';  // 装饰器
 
-  @Component({
-    props: {
-      propMessage:String
-    }
-  })
-
+  @Component
   export default class Types extends Vue {
     type = '-' // - 表示支出， + 表示收入.
-    helloMsg = 'Hello, ' + this.propMessage;
+
+    //@Prop(Number) xxx: number | undefined;
+    // Prop 告诉 Vue xxx 不是 data 是 Prop
+    //装饰器 Number Vue xxx 运行时是个Number
+    // xxx 属性名
+    // number | undefined 告诉 TS xxx 的编译时类型
 
     selectType(type: string) {
       if (type !== '-' && type !== '+') {
@@ -30,7 +30,6 @@
       }
       this.type = type;
     }
-
   }
 
   // export default {  // JS写法
